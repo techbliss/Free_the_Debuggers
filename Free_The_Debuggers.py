3ee0830d10bd874cae900969d5e48d8b2facff3d
Storm Shadow <Storm_Shadow@techbliss.org>
2014-08-15 21:00:51 +0200

    Update Free_The_Debuggers.py
    update to  ida 6,5

--- Free_The_Debuggers.py
+++ Free_The_Debuggers.py
@@ -1,10 +1,26 @@
 import sys
 import idaapi
-from idaapi import *
 import idc
-from idc import  *
 
-class plugin_t():
+
+class Freesome(idaapi.plugin_t):
+    flags = idaapi.PLUGIN_PROC
+    comment = ""
+
+    help = ""
+    wanted_name = "Free the Debugger"
+    wanted_hotkey = "Alt-F6"
+
+    def init(self):
+        idaapi.msg("Free the Debugger is found. \n")
+        return idaapi.PLUGIN_OK
+
+    def run(self, arg):
+        idaapi.msg("run() called with %d!\n" % arg)
+
+    def term(self):
+        idaapi.msg("")
+
 
 ##'lets give this baby some debuggers'''
     idaapi.load_and_run_plugin("windbg_user.plw", 0)
@@ -16,16 +32,11 @@ class plugin_t():
     idaapi.load_and_run_plugin("win32_user.plw", 0)
     idaapi.load_and_run_plugin("wince_stub.plw", 0)
     idaapi.load_and_run_plugin("bdescr.plw", 0)
-    idaapi.load_and_run_plugin("fentanyl.py", 0)
     idaapi.load_and_run_plugin("epoc_user", 0)
-    idaapi.load_and_run_plugin("deci3dbg.plw", 0)
-    idaapi.load_and_run_plugin("deci3dbg.p64", 0)
     idc.LoadDebugger("gdb", 1)
 
-
-
-# ----------------------------------------------------------------------
-def Plugin_ENTRY():
-  return plugin_t()
+	
+def PLUGIN_ENTRY():
+	return Freesome()
 
 print "Finally We Are Free At Last"
